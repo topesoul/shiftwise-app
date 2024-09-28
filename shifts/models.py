d4a5d843a6e80371d69d1b0676081e69e5baa610
Temitope Akingbala <topesoul@gmail.com>
2024-09-28 15:05:47 +0000

    Improve flexibility of Shift model attributes

--- shifts/models.py
+++ shifts/models.py
@@ -5,41 +5,36 @@ from geopy.distance import geodesic
 
 # Create your models here.
 class Shift(models.Model):
-    # Shift Name
-    name = models.CharField(max_length=100)
+    # Name of the shift
+    name = models.CharField(max_length=100, null=True)
 
-    # Mandatory fields for managing shift times
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    shift_date = models.DateField()
-
-    # Address fields for shift location
-    postcode = models.CharField(max_length=10)
-    address_line1 = models.CharField(max_length=255)
+    # Date and time fields
+    start_time = models.TimeField(null=True)
+    end_time = models.TimeField(null=True)
+    shift_date = models.DateField(null=True)
+    
+    # Address fields
+    postcode = models.CharField(max_length=10, null=True)
+    address_line1 = models.CharField(max_length=255, blank=True, null=True)
     address_line2 = models.CharField(max_length=255, blank=True, null=True)
-    city = models.CharField(max_length=100)
+    city = models.CharField(max_length=100, null=True)
     county = models.CharField(max_length=100, blank=True, null=True)
     country = models.CharField(max_length=100, default='UK')
 
-    # Location fields for proximity calculation
+    # Location fields for proximity checks
     latitude = models.FloatField(null=True, blank=True, validators=[MinValueValidator(-90.0), MaxValueValidator(90.0)])
     longitude = models.FloatField(null=True, blank=True, validators=[MinValueValidator(-180.0), MaxValueValidator(180.0)])
-
-    # If a shift requires specific skills or qualifications
-    required_skills = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} on {self.shift_date}"
 
-    # Function to calculate the distance from staff to the shift location
     def calculate_distance(self, worker_latitude, worker_longitude):
         if self.latitude and self.longitude and worker_latitude and worker_longitude:
             shift_location = (self.latitude, self.longitude)
             worker_location = (worker_latitude, worker_longitude)
-            return geodesic(shift_location, worker_location).miles  # Returns the distance in miles
+            return geodesic(shift_location, worker_location).miles
         return None
 
-    # Ensure the shift time is valid
     def clean(self):
-        if self.end_time <= self.start_time:
+        if self.end_time and self.start_time and self.end_time <= self.start_time:
             raise ValidationError('End time must be after the start time.')
