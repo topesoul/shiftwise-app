68a9cd247205bd0bcbc7722cb4195e357f1f07d0
Temitope Akingbala <topesoul@gmail.com>
2024-09-28 12:20:07 +0000

    feat: add shift list view to display all shifts

--- shifts/views.py
+++ shifts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Shift
+
+def shift_list(request):
+    shifts = Shift.objects.all()
+    return render(request, 'shifts/shift_list.html', {'shifts': shifts})
