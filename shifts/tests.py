9dcf60c40cd3d0c5df0625d89cbdee0a5d113d5c
Temitope Akingbala <topesoul@gmail.com>
2024-09-28 11:47:21 +0000

    Added initial test for shift creation

--- shifts/tests.py
+++ shifts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+# tests
+from django.test import TestCase
+from .models import Shift
+
+class ShiftTestCase(TestCase):
+    def test_shift_creation(self):
+        shift = Shift.objects.create(name="Morning Shift", start_time="08:00", end_time="12:00")
+        self.assertEqual(shift.name, "Morning Shift")
