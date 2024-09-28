a92848763b8104f50d07e30104c40db3cc1796e8
Temitope Akingbala <topesoul@gmail.com>
2024-09-28 11:49:35 +0000

    feat: Implemented Shift model and applied migrations

--- shifts/models.py
+++ shifts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Shift(models.Model):
+    name = models.CharField(max_length=100)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return self.name
