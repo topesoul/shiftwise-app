bb9d07b354b682f16018555a5185a0c4fa6b9fd7
Temitope Akingbala <topesoul@gmail.com>
2024-09-28 12:23:27 +0000

    feat: Add shift creation and update functionality

--- shifts/views.py
+++ shifts/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ShiftForm
 from .models import Shift
 
 def shift_list(request):
     shifts = Shift.objects.all()
     return render(request, 'shifts/shift_list.html', {'shifts': shifts})
+
+def shift_create(request):
+    if request.method == 'POST':
+        form = ShiftForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('shift_list')
+    else:
+        form = ShiftForm()
+    return render(request, 'shifts/shift_form.html', {'form': form})
+
+def shift_update(request, pk):
+    shift = Shift.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ShiftForm(request.POST, instance=shift)
+        if form.is_valid():
+            form.save()
+            return redirect('shift_list')
+    else:
+        form = ShiftForm(instance=shift)
+    return render(request, 'shifts/shift_form.html', {'form': form})
