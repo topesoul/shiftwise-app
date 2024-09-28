501280d8325d7b59fc5a169b86db09655b5b85e5
Temitope Akingbala <topesoul@gmail.com>
2024-09-28 14:31:27 +0000

    feat: add address and date fields to Shift model

--- shifts/models.py
+++ shifts/models.py
@@ -7,6 +7,15 @@ class Shift(models.Model):
     name = models.CharField(max_length=100)
     start_time = models.TimeField()
     end_time = models.TimeField()
+    shift_date = models.DateField()
+    postcode = models.CharField(max_length=10, blank=True, null=True)
+    address_line1 = models.CharField(max_length=255, blank=True, null=True)
+    address_line2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    county = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} on {self.shift_date}"
