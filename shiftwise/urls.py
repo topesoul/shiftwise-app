bbb34c1734812f00ca439a7b5221ec0d18e28aea
Temitope Akingbala <topesoul@gmail.com>
2024-10-05 18:31:03 +0000

    feat: add accounts URL pattern for better routing structure

--- shiftwise/urls.py
+++ shiftwise/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')), 
     path('shifts/', include('shifts.urls')),
     path('accounts/', include('allauth.urls')),
 ]
