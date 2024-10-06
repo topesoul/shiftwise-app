1bbd78415189b01ba6f4c85ccfb51fa0749914f9
Temitope Akingbala <topesoul@gmail.com>
2024-10-06 11:42:40 +0000

    feat: add Shift model admin interface

--- shifts/admin.py
+++ shifts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Shift
+from .forms import ShiftForm
 
 # Register your models here.
+@admin.register(Shift)
+class ShiftAdmin(admin.ModelAdmin):
+    form = ShiftForm
+    list_display = ('name', 'shift_date', 'start_time', 'end_time', 'city')
+    search_fields = ('name', 'city', 'postcode')
+    list_filter = ('shift_date', 'city', 'country')
