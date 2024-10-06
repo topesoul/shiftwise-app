7a14de490c1cda34a78594f6b143a33d91d2f9b9
Temitope Akingbala <topesoul@gmail.com>
2024-10-06 17:37:01 +0000

    feat: add profile model to extend user information

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from shifts.models import Agency
 
-# Create your models here.
+class Profile(models.Model):
+    """
+    Extends the User model with additional information.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    agency = models.ForeignKey(Agency, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
