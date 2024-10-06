2d47d115388b2ae76f8696990db19e7b6a0905f0
Temitope Akingbala <topesoul@gmail.com>
2024-10-06 16:22:33 +0000

    feat: remove unwanted comments from script

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+# accounts/views.py
 def custom_login(request):
-    # Point to the allauth login template
     return render(request, 'allauth/account/login.html')
 
 def custom_signup(request):
-    # Point to the allauth signup template
     return render(request, 'allauth/account/signup.html')
 
-# For custom profile template
+
 def profile(request):
     return render(request, 'accounts/profile.html')
