b0bab9839065dcb4f1ad06334db6a692e5260655
Temitope Akingbala <topesoul@gmail.com>
2024-10-06 15:26:44 +0000

    feat: refactor view imports for clarity

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-
 def custom_login(request):
     # Point to the allauth login template
     return render(request, 'allauth/account/login.html')
