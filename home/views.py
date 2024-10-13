068e7f869f017447827e2ab8cd13e84a0f4f8bf1
Temitope Akingbala <topesoul@gmail.com>
2024-10-13 12:57:46 +0000

    feat: rename index view and update template reference

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request, 'home/index.html')
+def home_view(request):
+    return render(request, 'home.html')
