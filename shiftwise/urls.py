906973cdf455c48794cc548863d90c1397e79bc2
Temitope Akingbala <topesoul@gmail.com>
2024-10-13 11:58:10 +0000

    feat: reordered URL patterns for clarity

--- shiftwise/urls.py
+++ shiftwise/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')), 
-    path('shifts/', include('shifts.urls')),
     path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('shifts/', include('shifts.urls')),
 ]
