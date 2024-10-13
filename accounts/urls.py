5bc94ec91e226b5996f539eae3eb920413834d4a
Temitope Akingbala <topesoul@gmail.com>
2024-10-13 20:26:25 +0000

    feat: Refactor signup URL to avoid duplication

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('signup/', views.signup_view, name='account_signup'),
     path('profile/', views.profile_view, name='account_profile'),
     path('login/', views.login_view, name='account_login'),
     path('logout/', views.logout_view, name='account_logout'),
-    path('signup/', views.signup_view, name='account_signup'),
 ]
