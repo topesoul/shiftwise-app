0ef3e56867bbe3ab6194c29548522711905fb39f
Temitope Akingbala <topesoul@gmail.com>
2024-10-13 16:13:30 +0000

    feat: add namespacing to account URLs for clarity

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('profile/', views.profile_view, name='account_profile'),
     path('login/', views.login_view, name='account_login'),
