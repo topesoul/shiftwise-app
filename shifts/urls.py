8c99135162c51e7bfedccf9d33e1049135ed20ee
Temitope Akingbala <topesoul@gmail.com>
2024-10-13 20:28:29 +0000

    feat: Add booking functionality to shifts URL configuration

--- shifts/urls.py
+++ shifts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import ShiftListView, ShiftCreateView, ShiftUpdateView, ShiftDeleteView
+from .views import ShiftListView, ShiftCreateView, ShiftUpdateView, ShiftDeleteView, book_shift
+
+app_name = 'shifts'
 
 urlpatterns = [
     path('', ShiftListView.as_view(), name='shift_list'),
     path('create/', ShiftCreateView.as_view(), name='shift_create'),
     path('<int:pk>/edit/', ShiftUpdateView.as_view(), name='shift_update'),
     path('<int:pk>/delete/', ShiftDeleteView.as_view(), name='shift_delete'),
+    path('<int:shift_id>/book/', book_shift, name='book_shift'),
 ]
