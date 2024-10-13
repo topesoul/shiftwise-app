50a538263dff9ac72eb6fffb6dc14036d027b9fd
Temitope Akingbala <topesoul@gmail.com>
2024-10-13 12:51:02 +0000

    feat: update URL routing to include home page

--- shiftwise/urls.py
+++ shiftwise/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/', include('accounts.urls')),
     path('shifts/', include('shifts.urls')),
+    path('', include('home.urls')),
 ]
+
