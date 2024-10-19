3316a304a2a0c615a44603bf903109e02e161e37
Temitope Akingbala <topesoul@gmail.com>
2024-10-19 09:34:42 +0000

    feat: Add Profile inline to User admin for improved usability

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Profile
+
+
+class ProfileInline(admin.StackedInline):
+    """
+    Defines an inline admin descriptor for Profile model
+    which acts a bit like a singleton
+    """
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class UserAdmin(BaseUserAdmin):
+    """
+    Define a new User admin which includes Profile inline
+    """
+    inlines = (ProfileInline, )
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'agency')
+    list_select_related = ('profile', )
+
+    def agency(self, instance):
+        return instance.profile.agency
+    agency.short_description = 'Agency'
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 
 @admin.register(Profile)
