af2eaff5159f4e84bac3219a35c82e86b583a033
Temitope Akingbala <topesoul@gmail.com>
2024-10-19 09:29:36 +0000

    feat: improve model relationships with related names

--- accounts/models.py
+++ accounts/models.py
@@ -6,8 +6,8 @@ class Profile(models.Model):
     """
     Extends the User model with additional information.
     """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    agency = models.ForeignKey(Agency, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    agency = models.ForeignKey(Agency, on_delete=models.SET_NULL, null=True, blank=True, related_name='users')
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
