801a650ca8c50f038e9b04043cc6c7b3f0a6ab7c
Temitope Akingbala <topesoul@gmail.com>
2024-10-19 09:55:34 +0000

    feat: Add shift detail and address management routes

--- shifts/urls.py
+++ shifts/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import ShiftListView, ShiftCreateView, ShiftUpdateView, ShiftDeleteView, book_shift
+from .views import (
+    ShiftListView,
+    ShiftCreateView,
+    ShiftUpdateView,
+    ShiftDeleteView,
+    ShiftDetailView,
+    book_shift,
+    unbook_shift,
+    get_address,
+)
 
 app_name = 'shifts'
 
@@ -8,5 +17,8 @@ urlpatterns = [
     path('create/', ShiftCreateView.as_view(), name='shift_create'),
     path('<int:pk>/edit/', ShiftUpdateView.as_view(), name='shift_update'),
     path('<int:pk>/delete/', ShiftDeleteView.as_view(), name='shift_delete'),
+    path('<int:pk>/', ShiftDetailView.as_view(), name='shift_detail'),
     path('<int:shift_id>/book/', book_shift, name='book_shift'),
+    path('<int:shift_id>/unbook/', unbook_shift, name='unbook_shift'),
+    path('get_address/', get_address, name='get_address'),
 ]
