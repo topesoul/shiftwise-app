289b0577960b323ef273ca772f80287230183205
Temitope Akingbala <topesoul@gmail.com>
2024-10-19 15:56:04 +0000

    feat: Refactor URL imports for consistency

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import home_view
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('', home_view, name='home'),
 ]
