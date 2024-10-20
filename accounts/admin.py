7a0fd5c186aa6032b5d33ab5c7c4f69b370fb819
Temitope Akingbala <topesoul@gmail.com>
2024-10-20 08:50:49 +0000

    feat: Enhance User Admin to improve profile management

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,41 +1,28 @@
+# /workspace/shiftwise/accounts/admin.py
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import Profile
 
+User = get_user_model()
 
 class ProfileInline(admin.StackedInline):
-    """
-    Defines an inline admin descriptor for Profile model
-    which acts a bit like a singleton
-    """
     model = Profile
     can_delete = False
     verbose_name_plural = 'Profile'
     fk_name = 'user'
 
-
 class UserAdmin(BaseUserAdmin):
-    """
-    Define a new User admin which includes Profile inline
-    """
     inlines = (ProfileInline, )
-
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'agency')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role', 'agency')
     list_select_related = ('profile', )
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'role')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
 
     def agency(self, instance):
         return instance.profile.agency
     agency.short_description = 'Agency'
 
-
-# Re-register UserAdmin
-admin.site.unregister(User)
+# Register the custom UserAdmin
 admin.site.register(User, UserAdmin)
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'agency')
-    search_fields = ('user__username', 'agency__name')
-    list_filter = ('agency',)
