e4ef15732ab892ca869f0c465b038d7a7aa7fe48
Temitope Akingbala <topesoul@gmail.com>
2024-10-20 08:54:25 +0000

    feat: Refactor user profile management in signals

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,22 +1,15 @@
+# accounts/signals.py
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import Profile
 
+User = get_user_model()
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """
-    Creates a Profile instance whenever a new User is created.
-    """
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    """
-    Saves the Profile instance whenever the User instance is saved.
-    """
-    if hasattr(instance, 'profile'):
+    else:
         instance.profile.save()
