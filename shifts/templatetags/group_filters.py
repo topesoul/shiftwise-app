efa2682f52561855ce8d16c194ea52381013f2cc
Temitope Akingbala <topesoul@gmail.com>
2024-10-20 09:19:06 +0000

    feat: Refine documentation for group membership check

--- shifts/templatetags/group_filters.py
+++ shifts/templatetags/group_filters.py
@@ -5,6 +5,6 @@ register = template.Library()
 @register.filter(name='has_group')
 def has_group(user, group_name):
     """
-    Returns True if the user is part of the given group.
+    Returns True if the user belongs to the specified group.
     """
     return user.groups.filter(name=group_name).exists()
