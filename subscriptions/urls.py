544e234135be3a8249f75928d377f000d2109f4a
Temitope Akingbala <topesoul@gmail.com>
2024-11-11 19:11:07 +0000

    Subject: Add url patterns for subscription services

---
+++ subscriptions/urls.py
@@ -0,0 +1,14 @@
+# /workspace/shiftwise/subscriptions/urls.py
+
+from django.urls import path
+from . import views
+
+app_name = "subscriptions"
+
+urlpatterns = [
+    path("", views.SubscriptionHomeView.as_view(), name="subscription_home"),
+    path("subscribe/<int:plan_id>/", views.SubscribeView.as_view(), name="subscribe"),
+    path("success/", views.subscription_success, name="subscription_success"),
+    path("cancel/", views.subscription_cancel, name="subscription_cancel"),
+    path("webhook/", views.stripe_webhook, name="stripe_webhook"),
+]
