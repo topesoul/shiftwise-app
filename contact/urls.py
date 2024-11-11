3bf4496b8d619050993226d6ddc056ad6ded45cd
Temitope Akingbala <topesoul@gmail.com>
2024-11-11 20:00:20 +0000

    feat: Add contact page routing setup

---
+++ contact/urls.py
@@ -0,0 +1,10 @@
+# /workspace/shiftwise/contact/urls.py
+
+from django.urls import path
+from . import views
+
+app_name = "contact"
+
+urlpatterns = [
+    path("", views.contact_view, name="contact"),
+]
