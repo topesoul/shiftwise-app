0c6dae3e124b20fda0ef13854329f44cbdce5f6e
Temitope Akingbala <topesoul@gmail.com>
2024-11-11 20:04:12 +0000

    Subject: Refactor URL routing and add exception handlers

--- shiftwise/urls.py
+++ shiftwise/urls.py
@@ -1,25 +1,30 @@
-"""shiftwise URL Configuration
+# /workspace/shiftwise/shiftwise/urls.py
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
-    path('shifts/', include(('shifts.urls', 'shifts'), namespace='shifts')),
-    path('', include(('home.urls', 'home'), namespace='home')),
+    path("admin/", admin.site.urls),
+    path("shifts/", include("shifts.urls", namespace="shifts")),
+    path(
+        "accounts/", include(("accounts.urls", "accounts"), namespace="accounts")
+    ),  # Custom accounts
+    path("auth/", include("allauth.urls")),  # Allauth under 'auth/'
+    path("contact/", include("contact.urls", namespace="contact")),
+    path("subscriptions/", include("subscriptions.urls", namespace="subscriptions")),
+    path("", include("home.urls", namespace="home")),
 ]
+
+handler403 = "shifts.views.custom_permission_denied_view"
+handler404 = "shifts.views.custom_page_not_found_view"
+handler500 = "shifts.views.custom_server_error_view"
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
