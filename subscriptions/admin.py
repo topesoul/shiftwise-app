bebfd22584428cb9e4927e458cc97235d368385e
Temitope Akingbala <topesoul@gmail.com>
2024-11-11 19:38:30 +0000

    Subject: Introduce Plan and Subscription admins in Django

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,33 @@
-from django.contrib import admin
+# /workspace/shiftwise/subscriptions/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Plan, Subscription
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "view_limit",
+        "stripe_product_id",
+        "stripe_price_id",
+        "description",
+    )
+    search_fields = ("name", "stripe_product_id", "stripe_price_id", "description")
+    list_filter = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "agency",
+        "plan",
+        "is_active",
+        "current_period_start",
+        "current_period_end",
+        "stripe_subscription_id",
+    )
+    search_fields = ("agency__name", "plan__name", "stripe_subscription_id")
+    list_filter = ("is_active", "plan__name")
+    ordering = ("-current_period_start",)
