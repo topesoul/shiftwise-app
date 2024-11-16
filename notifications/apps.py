883d59f68be3806ac0d170461ae5c5a6cb56a1d8
Temitope Akingbala <topesoul@gmail.com>
2024-11-16 13:29:18 +0000

    feat: Introduce Notifications Django app

---
+++ notifications/apps.py
@@ -0,0 +1,7 @@
+# notifications/apps.py
+
+from django.apps import AppConfig
+
+class NotificationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'notifications'
