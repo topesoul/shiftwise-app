f7a533dd901e8ce3b2b051fd5f3b4428ed81f182
Temitope Akingbala <topesoul@gmail.com>
2024-11-16 13:37:56 +0000

    feat: Refactor subscription views imports and paths

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,16 +1,30 @@
 # /workspace/shiftwise/subscriptions/urls.py
 
 from django.urls import path
-from . import views
+from .views import (
+    SubscriptionHomeView,
+    SubscribeView,
+    ManageSubscriptionView,
+    CancelSubscriptionView,
+    UpdatePaymentMethodView,
+    UpgradeSubscriptionView,
+    DowngradeSubscriptionView,
+    subscription_success,
+    subscription_cancel,
+    stripe_webhook,
+)
 
-app_name = "subscriptions"
+app_name = 'subscriptions'
 
 urlpatterns = [
-    path("", views.SubscriptionHomeView.as_view(), name="subscription_home"),
-    path("subscribe/<int:plan_id>/", views.SubscribeView.as_view(), name="subscribe"),
-    path("success/", views.subscription_success, name="subscription_success"),
-    path("cancel/", views.subscription_cancel, name="subscription_cancel"),
-    path("webhook/", views.stripe_webhook, name="stripe_webhook"),
-    path("upgrade/", views.UpgradeSubscriptionView.as_view(), name="upgrade_subscription"),
-    path("downgrade/", views.DowngradeSubscriptionView.as_view(), name="downgrade_subscription"),
+    path('', SubscriptionHomeView.as_view(), name='subscription_home'),
+    path('subscribe/<int:plan_id>/', SubscribeView.as_view(), name='subscribe'),
+    path('manage/', ManageSubscriptionView.as_view(), name='manage_subscription'),
+    path('cancel/', CancelSubscriptionView.as_view(), name='cancel_subscription'),
+    path('update-payment/', UpdatePaymentMethodView.as_view(), name='update_payment_method'),
+    path('upgrade/', UpgradeSubscriptionView.as_view(), name='upgrade_subscription'),
+    path('downgrade/', DowngradeSubscriptionView.as_view(), name='downgrade_subscription'),
+    path('success/', subscription_success, name='subscription_success'),
+    path('cancelled/', subscription_cancel, name='subscription_cancel'),
+    path('webhook/', stripe_webhook, name='stripe_webhook'),
 ]
