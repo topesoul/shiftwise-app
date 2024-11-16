66d3accaf48ef55437a2b19ff559ed77991f623d
Temitope Akingbala <topesoul@gmail.com>
2024-11-16 22:36:38 +0000

    feat: Enhance address autocomplete functionality

--- static/js/address_autocomplete.js
+++ static/js/address_autocomplete.js
@@ -1,80 +1,56 @@
-// static/js/address_autocomplete.js
+// /workspace/shiftwise/static/js/address_autocomplete.js
 
 function initAutocomplete() {
-    const addressInput = document.getElementById('id_address_line1');
-    const autocomplete = new google.maps.places.Autocomplete(addressInput, {
-        types: ['geocode'],
-    });
+    const addressInputs = document.querySelectorAll('.address-autocomplete');
 
-    autocomplete.addListener('place_changed', function () {
-        const place = autocomplete.getPlace();
-        if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and pressed the Enter key
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-        }
+    addressInputs.forEach(function (addressInput) {
+        const autocomplete = new google.maps.places.Autocomplete(addressInput, {
+            types: ['geocode'],
+            componentRestrictions: { country: ['uk'] },
+        });
 
-        // Extract latitude and longitude
-        const latitude = place.geometry.location.lat();
-        const longitude = place.geometry.location.lng();
+        autocomplete.addListener('place_changed', function () {
+            const place = autocomplete.getPlace();
 
-        // Set the hidden fields
-        document.getElementById('id_latitude').value = latitude;
-        document.getElementById('id_longitude').value = longitude;
+            // Get the form fields
+            const form = addressInput.form;
+            const latitudeInput = form.querySelector('#id_latitude');
+            const longitudeInput = form.querySelector('#id_longitude');
+            const cityInput = form.querySelector('#id_city');
+            const countyInput = form.querySelector('#id_county');
+            const postcodeInput = form.querySelector('#id_postcode');
+            const countryInput = form.querySelector('#id_country');
 
-        // Extract address components
-        const addressComponents = place.address_components;
-        const componentForm = {
-            street_number: 'short_name',
-            route: 'long_name',
-            locality: 'long_name',
-            administrative_area_level_1: 'short_name',
-            country: 'long_name',
-            postal_code: 'short_name'
-        };
+            // Reset fields
+            if (cityInput) cityInput.value = '';
+            if (countyInput) countyInput.value = '';
+            if (postcodeInput) postcodeInput.value = '';
+            if (countryInput) countryInput.value = '';
 
-        // Reset existing address fields
-        document.getElementById('id_address_line1').value = '';
-        document.getElementById('id_address_line2').value = '';
-        document.getElementById('id_city').value = '';
-        document.getElementById('id_county').value = '';
-        document.getElementById('id_postcode').value = '';
-        document.getElementById('id_country').value = '';
+            // Set latitude and longitude
+            if (latitudeInput) latitudeInput.value = place.geometry.location.lat();
+            if (longitudeInput) longitudeInput.value = place.geometry.location.lng();
 
-        // Populate address fields
-        let streetNumber = '';
-        let route = '';
-
-        for (const component of addressComponents) {
-            const addressType = component.types[0];
-            if (componentForm[addressType]) {
-                const val = component[componentForm[addressType]];
-                switch (addressType) {
-                    case 'street_number':
-                        streetNumber = val;
-                        break;
-                    case 'route':
-                        route = val;
-                        break;
-                    case 'locality':
-                        document.getElementById('id_city').value = val;
-                        break;
-                    case 'administrative_area_level_1':
-                        document.getElementById('id_county').value = val;
-                        break;
-                    case 'postal_code':
-                        document.getElementById('id_postcode').value = val;
-                        break;
-                    case 'country':
-                        document.getElementById('id_country').value = val;
-                        break;
-                    default:
-                        break;
+            // Parse address components
+            for (const component of place.address_components) {
+                const types = component.types;
+                if (types.includes('locality')) {
+                    if (cityInput) cityInput.value = component.long_name;
+                } else if (types.includes('administrative_area_level_2')) {
+                    if (countyInput) countyInput.value = component.long_name;
+                } else if (types.includes('postal_code')) {
+                    if (postcodeInput) postcodeInput.value = component.long_name;
+                } else if (types.includes('country')) {
+                    if (countryInput) countryInput.value = component.long_name;
                 }
             }
-        }
-
-        // Combine street number and route for address_line1
-        document.getElementById('id_address_line1').value = streetNumber + ' ' + route;
+        });
     });
 }
+
+// Initialize autocomplete when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+    if (typeof google !== 'undefined') {
+        initAutocomplete();
+    }
+});
