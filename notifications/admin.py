2acf315f5be9ecfa49602890fba08468a2e39784
Temitope Akingbala <topesoul@gmail.com>
2024-11-16 13:29:01 +0000

    feat: Introduces Notification administration interface

---
+++ notifications/admin.py
@@ -0,0 +1,10 @@
+# notifications/admin.py
+
+from django.contrib import admin
+from .models import Notification
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'read', 'created_at')
+    list_filter = ('read', 'created_at')
+    search_fields = ('user__username', 'message')
