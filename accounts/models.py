cfc3d846b10427320936270328e4e0450513cb06
Temitope Akingbala <topesoul@gmail.com>
2024-11-18 02:19:44 +0000

    feat: Ensure unique user emails.

--- accounts/models.py
+++ accounts/models.py
@@ -22,6 +22,7 @@ class User(AbstractUser):
         ("agency_owner", "Agency Owner"),
     )
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="staff")
+    email = models.EmailField(max_length=254, unique=True)  # Ensuring unique emails
 
     def __str__(self):
         return self.username
