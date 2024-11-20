6b53e02f95ab74244707adea86542bf08b2aba7d
Temitope Akingbala <topesoul@gmail.com>
2024-11-20 22:54:02 +0000

    feat: streamline subscriptions URL patterns

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,54 +1,30 @@
 # /workspace/shiftwise/subscriptions/urls.py
 
 from django.urls import path
-
 from .views import (
-    CancelSubscriptionView,
-    DowngradeSubscriptionView,
-    ManageSubscriptionView,
-    SubscribeView,
     SubscriptionHomeView,
-    UpdatePaymentMethodView,
-    UpgradeSubscriptionView,
-    StripeWebhookView,
-    subscription_cancel,
+    SubscribeView,
     subscription_success,
+    subscription_cancel,
+    StripeWebhookView,
+    UpgradeSubscriptionView,
+    DowngradeSubscriptionView,
+    CancelSubscriptionView,
+    ManageSubscriptionView,
+    UpdatePaymentMethodView,
 )
 
-app_name = "subscriptions"
+app_name = 'subscriptions'
 
 urlpatterns = [
-    # Subscription Home
-    path("", SubscriptionHomeView.as_view(), name="subscription_home"),
-
-    # Subscribe to a Plan
-    path("subscribe/<int:plan_id>/", SubscribeView.as_view(), name="subscribe"),
-
-    # Manage Subscription
-    path("manage/", ManageSubscriptionView.as_view(), name="manage_subscription"),
-
-    # Cancel Subscription
-    path("cancel/", CancelSubscriptionView.as_view(), name="cancel_subscription"),
-
-    # Update Payment Method
-    path(
-        "update-payment/",
-        UpdatePaymentMethodView.as_view(),
-        name="update_payment_method",
-    ),
-
-    # Upgrade Subscription
-    path("upgrade/", UpgradeSubscriptionView.as_view(), name="upgrade_subscription"),
-
-    # Downgrade Subscription
-    path(
-        "downgrade/", DowngradeSubscriptionView.as_view(), name="downgrade_subscription"
-    ),
-
-    # Subscription Success and Cancellation Pages
-    path("success/", subscription_success, name="subscription_success"),
-    path("cancelled/", subscription_cancel, name="subscription_cancel"),
-
-    # Stripe Webhook
-    path("webhook/", StripeWebhookView.as_view(), name="stripe_webhook"),
+    path('', SubscriptionHomeView.as_view(), name='subscription_home'),
+    path('subscribe/<int:plan_id>/', SubscribeView.as_view(), name='subscribe'),
+    path('success/', subscription_success, name='subscription_success'),
+    path('cancelled/', subscription_cancel, name='subscription_cancel'),
+    path('webhook/', StripeWebhookView.as_view(), name='stripe_webhook'),
+    path('upgrade/<int:plan_id>/', UpgradeSubscriptionView.as_view(), name='upgrade_subscription'),
+    path('downgrade/<int:plan_id>/', DowngradeSubscriptionView.as_view(), name='downgrade_subscription'),
+    path('cancel/', CancelSubscriptionView.as_view(), name='cancel_subscription'),
+    path('manage/', ManageSubscriptionView.as_view(), name='manage_subscription'),
+    path('update_payment_method/', UpdatePaymentMethodView.as_view(), name='update_payment_method'),
 ]
