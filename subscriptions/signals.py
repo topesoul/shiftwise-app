9e4b72fe1e06a67596d7ac927af562710f0862e3
Temitope Akingbala <topesoul@gmail.com>
2024-11-20 23:51:12 +0000

    feat: Add timezone utility import for agency creation

--- subscriptions/signals.py
+++ subscriptions/signals.py
@@ -4,12 +4,14 @@ import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import transaction
+from django.utils import timezone
 
 from subscriptions.utils import create_stripe_customer
 from .models import Subscription, Plan
 from accounts.models import Agency
 
 logger = logging.getLogger(__name__)
+
 
 @receiver(post_save, sender=Agency)
 def handle_agency_creation(sender, instance, created, **kwargs):
