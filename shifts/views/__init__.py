5a7440a021e691e6ab18396de556841ada96301a
Temitope Akingbala <topesoul@gmail.com>
2024-11-24 22:25:13 +0000

    feat: Refactor import order in views initialization

--- shifts/views/__init__.py
+++ shifts/views/__init__.py
@@ -2,115 +2,94 @@
 
 __all__ = [
     # Staff Management Views
-    'StaffListView',
-    'StaffCreateView',
-    'StaffUpdateView',
-    'StaffDeleteView',
-
+    "StaffListView",
+    "StaffCreateView",
+    "StaffUpdateView",
+    "StaffDeleteView",
     # Shift Management Views
-    'ShiftListView',
-    'ShiftCreateView',
-    'ShiftUpdateView',
-    'ShiftDeleteView',
-    'ShiftDetailView',
-
+    "ShiftListView",
+    "ShiftCreateView",
+    "ShiftUpdateView",
+    "ShiftDeleteView",
+    "ShiftDetailView",
     # Shift Completion Views
-    'ShiftCompleteView',
-    'ShiftCompleteForUserView',
-    'ShiftCompleteAjaxView',
-
+    "ShiftCompleteView",
+    "ShiftCompleteForUserView",
+    "ShiftCompleteAjaxView",
     # Shift Booking Views
-    'ShiftBookView',
-    'ShiftUnbookView',
-
+    "ShiftBookView",
+    "ShiftUnbookView",
     # Staff Performance Views
-    'StaffPerformanceListView',
-    'StaffPerformanceDetailView',
-    'StaffPerformanceCreateView',
-    'StaffPerformanceUpdateView',
-    'StaffPerformanceDeleteView',
-
+    "StaffPerformanceListView",
+    "StaffPerformanceDetailView",
+    "StaffPerformanceCreateView",
+    "StaffPerformanceUpdateView",
+    "StaffPerformanceDeleteView",
     # Reporting Views
-    'TimesheetDownloadView',
-    'ReportDashboardView',
-
+    "TimesheetDownloadView",
+    "ReportDashboardView",
     # Assignment Views
-    'AssignWorkerView',
-    'UnassignWorkerView',
-
+    "AssignWorkerView",
+    "UnassignWorkerView",
     # API Views
-    'ShiftDetailsAPIView',
-    'APIAccessView',
-
+    "ShiftDetailsAPIView",
+    "APIAccessView",
     # Dashboard Views
-    'DashboardView',
-
+    "DashboardView",
     # Custom Views
-    'custom_permission_denied_view',
-    'custom_page_not_found_view',
-    'custom_server_error_view',
+    "custom_permission_denied_view",
+    "custom_page_not_found_view",
+    "custom_server_error_view",
 ]
 
-# Staff Management Views
-from .staff_views import (
-    StaffListView,
-    StaffCreateView,
-    StaffUpdateView,
-    StaffDeleteView,
+# Custom Views
+from shifts.views.custom_views import (
+    custom_page_not_found_view,
+    custom_permission_denied_view,
+    custom_server_error_view,
 )
 
-# Shift Management Views
-from .shift_views import (
-    ShiftListView,
-    ShiftCreateView,
-    ShiftUpdateView,
-    ShiftDeleteView,
-    ShiftDetailView,
-)
+# API Views
+from .api_views import APIAccessView, ShiftDetailsAPIView
+
+# Assignment Views
+from .assignment_views import AssignWorkerView, UnassignWorkerView
+
+# Shift Booking Views
+from .booking_views import ShiftBookView, ShiftUnbookView
 
 # Shift Completion Views
 from .completion_views import (
-    ShiftCompleteView,
-    ShiftCompleteForUserView,
     ShiftCompleteAjaxView,
-)
-
-# Shift Booking Views
-from .booking_views import (
-    ShiftBookView,
-    ShiftUnbookView,
+    ShiftCompleteForUserView,
+    ShiftCompleteView,
 )
 
 # Staff Performance Views
 from .performance_views import (
-    StaffPerformanceListView,
-    StaffPerformanceDetailView,
     StaffPerformanceCreateView,
-    StaffPerformanceUpdateView,
     StaffPerformanceDeleteView,
+    StaffPerformanceDetailView,
+    StaffPerformanceListView,
+    StaffPerformanceUpdateView,
 )
 
 # Reporting Views
-from .report_views import (
-    TimesheetDownloadView,
-    ReportDashboardView,
+from .report_views import ReportDashboardView, TimesheetDownloadView
+
+# Shift Management Views
+from .shift_views import (
+    ShiftCreateView,
+    ShiftDeleteView,
+    ShiftDetailView,
+    ShiftListView,
+    ShiftUpdateView,
 )
 
-# Assignment Views
-from .assignment_views import (
-    AssignWorkerView,
-    UnassignWorkerView,
-)
-
-# API Views
-from .api_views import (
-    ShiftDetailsAPIView,
-    APIAccessView,
-)
-
-# Custom Views
-from shifts.views.custom_views import (
-    custom_permission_denied_view,
-    custom_page_not_found_view,
-    custom_server_error_view,
+# Staff Management Views
+from .staff_views import (
+    StaffCreateView,
+    StaffDeleteView,
+    StaffListView,
+    StaffUpdateView,
 )
