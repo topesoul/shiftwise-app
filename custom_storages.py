c6d7cef7a9d347715b047874bf738a128deb68c2
Temitope Akingbala <topesoul@gmail.com>
2024-11-24 02:53:04 +0000

    refactor: clean up storage ACL comments

--- custom_storages.py
+++ custom_storages.py
@@ -1,14 +1,14 @@
-# /workspace/shiftwise/custom_storages.py
+# custom_storages.py
 
-from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    default_acl = None  # Disable ACLs
-    file_overwrite = True  # Allow overwriting of static files
+    default_acl = None
+    file_overwrite = True
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    default_acl = None  # Disable ACLs
-    file_overwrite = False  # Prevent overwriting of media files
+    default_acl = None
+    file_overwrite = False
