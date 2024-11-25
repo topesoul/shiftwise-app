7de4368cbeacd9158e4fc9daf412cb9602a26ebe
Temitope Akingbala <topesoul@gmail.com>
2024-11-25 16:00:26 +0000

    chore: remove unused code for key management and modals

--- static/js/modal_control.js
+++
@@ -1,8 +0,0 @@
-// /workspace/shiftwise/static/js/modal_control.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Check if the modal should be opened
-    if (window.OPEN_MODAL) {
-        $('#updateProfileModal').modal('show');
-    }
-});
