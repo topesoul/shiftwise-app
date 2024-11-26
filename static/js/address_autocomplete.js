172b55aef4ef3e92de4308d7eb1583bf63342f06
Temitope Akingbala <topesoul@gmail.com>
2024-11-26 00:48:43 +0000

    feat: add robust validation for address autocomplete

--- static/js/address_autocomplete.js
+++ static/js/address_autocomplete.js
@@ -1,107 +1,120 @@
-// /workspace/shiftwise/static/js/address_autocomplete.js
+// static/js/address_autocomplete.js
 
+// static/js/address_autocomplete.js
+
+// Ensure `initAutocomplete` is globally accessible for Google Maps API
+window.initAutocomplete = function () {
+    console.log("Google Maps API loaded successfully.");
+
+    // Check for jQuery
+    if (typeof $ === "undefined") {
+        console.error("jQuery is not loaded. Ensure jQuery is included before this script.");
+        return;
+    }
+
+    // Select all inputs with the class 'address-autocomplete'
+    const $addressInputs = $("input.address-autocomplete");
+    console.log(`Found ${$addressInputs.length} address-autocomplete inputs.`);
+
+    $addressInputs.each(function () {
+        const input = this;
+        const inputId = $(input).attr("id");
+        console.log(`Initializing autocomplete for input ID: ${inputId}`);
+
+        // Extract base ID from the input's ID
+        const baseIdMatch = inputId.match(/^(.*)address_line1$/);
+        if (!baseIdMatch) {
+            console.error(`Input ID "${inputId}" does not end with 'address_line1'.`);
+            return;
+        }
+        const baseId = baseIdMatch[1];
+
+        // Initialize Google Places Autocomplete
+        const autocomplete = new google.maps.places.Autocomplete(input, {
+            types: ["geocode"],
+            componentRestrictions: { country: ["uk"] },
+        });
+
+        // Specify fields to retrieve from Google Places
+        autocomplete.setFields(["address_component", "geometry"]);
+
+        // Handle place selection
+        autocomplete.addListener("place_changed", function () {
+            console.log(`Place changed for input ID: ${inputId}`);
+            const place = autocomplete.getPlace();
+
+            if (place.geometry && place.geometry.location) {
+                console.log(
+                    `Geocoded location: ${place.geometry.location.lat()}, ${place.geometry.location.lng()}`
+                );
+
+                // Define mappings of field names to input IDs
+                const fieldMap = {
+                    address_line1: baseId + "address_line1",
+                    address_line2: baseId + "address_line2",
+                    city: baseId + "city",
+                    county: baseId + "county",
+                    postcode: baseId + "postcode",
+                    country: baseId + "country",
+                    latitude: baseId + "latitude",
+                    longitude: baseId + "longitude",
+                };
+
+                // Helper function to set field values
+                const setField = (fieldId, value) => $(`#${fieldId}`).val(value);
+
+                // Reset all fields
+                Object.keys(fieldMap).forEach((key) => setField(fieldMap[key], ""));
+
+                // Populate fields with Google Places components
+                place.address_components.forEach((component) => {
+                    const componentType = component.types[0];
+                    switch (componentType) {
+                        case "street_number":
+                            setField(fieldMap.address_line1, component.long_name + " ");
+                            break;
+                        case "route":
+                            setField(
+                                fieldMap.address_line1,
+                                $(`#${fieldMap.address_line1}`).val() + component.long_name
+                            );
+                            break;
+                        case "locality":
+                            setField(fieldMap.city, component.long_name);
+                            break;
+                        case "administrative_area_level_2":
+                            setField(fieldMap.county, component.long_name);
+                            break;
+                        case "postal_code":
+                            setField(fieldMap.postcode, component.long_name);
+                            break;
+                        case "country":
+                            setField(fieldMap.country, component.long_name);
+                            break;
+                    }
+                });
+
+                // Set latitude and longitude
+                setField(fieldMap.latitude, place.geometry.location.lat());
+                setField(fieldMap.longitude, place.geometry.location.lng());
+            } else {
+                console.error("No geometry available for selected place.");
+            }
+        });
+    });
+};
+
+// Wait for Google Maps API and dependencies to load before initializing
+function ensureDependenciesAndInitialize() {
+    if (typeof google === "undefined" || typeof google.maps === "undefined") {
+        console.error("Google Maps API is not loaded. Retrying...");
+        setTimeout(ensureDependenciesAndInitialize, 100); // Retry after 100ms
+    } else {
+        initAutocomplete();
+    }
+}
+
+// Run initialization when the DOM is ready
 $(document).ready(function () {
-  console.log("initAutocomplete function called.");
-
-  // Select all input fields with the 'address-autocomplete' class
-  const $addressInputs = $('input.address-autocomplete');
-  console.log(`Found ${$addressInputs.length} address-autocomplete inputs.`);
-
-  $addressInputs.each(function () {
-      const input = this;
-      const inputId = $(input).attr('id');
-      console.log(`Initializing autocomplete for input ID: ${inputId}`);
-
-      // Determine the base ID by removing 'address_line1' from the input's ID
-      const baseIdMatch = inputId.match(/^(.*)address_line1$/);
-      if (!baseIdMatch) {
-          console.error(`Input ID "${inputId}" does not end with 'address_line1'.`);
-          return;
-      }
-      const baseId = baseIdMatch[1];  // e.g., 'id_shift_' or 'id_profile_'
-
-      // Initialize Google Places Autocomplete
-      const autocomplete = new google.maps.places.Autocomplete(input, {
-          types: ['geocode'], // Restrict results to addresses
-          componentRestrictions: { country: ['uk'] } // Restrict to UK addresses
-      });
-
-      // Specify the fields to retrieve from the Place object
-      autocomplete.setFields(['address_component', 'geometry']);
-
-      // Add listener for place selection
-      autocomplete.addListener('place_changed', function () {
-          console.log(`Place changed for input ID: ${inputId}`);
-          const place = autocomplete.getPlace();
-
-          if (place.geometry && place.geometry.location) {
-              console.log(`Geocoded location: ${place.geometry.location.lat()}, ${place.geometry.location.lng()}`);
-
-              // Construct IDs for address fields
-              const addressLine1Id = baseId + 'address_line1';
-              const addressLine2Id = baseId + 'address_line2';
-              const cityId = baseId + 'city';
-              const countyId = baseId + 'county';
-              const postcodeId = baseId + 'postcode';
-              const countryId = baseId + 'country';
-              const latFieldId = baseId + 'latitude';
-              const lngFieldId = baseId + 'longitude';
-
-              // Get the address fields using jQuery
-              const $addressLine1Field = $('#' + addressLine1Id);
-              const $addressLine2Field = $('#' + addressLine2Id);
-              const $cityField = $('#' + cityId);
-              const $countyField = $('#' + countyId);
-              const $postcodeField = $('#' + postcodeId);
-              const $countryField = $('#' + countryId);
-              const $latField = $('#' + latFieldId);
-              const $lngField = $('#' + lngFieldId);
-
-              // Reset the fields
-              $addressLine1Field.val('');
-              $addressLine2Field.val('');
-              $cityField.val('');
-              $countyField.val('');
-              $postcodeField.val('');
-              $countryField.val('UK'); // Default to UK
-
-              // Get each component of the address from the place details
-              place.address_components.forEach(function (component) {
-                  const componentType = component.types[0];
-
-                  switch (componentType) {
-                      case 'street_number':
-                          $addressLine1Field.val(component.long_name + ' ' + $addressLine1Field.val());
-                          break;
-                      case 'route':
-                          $addressLine1Field.val($addressLine1Field.val() + component.long_name);
-                          break;
-                      case 'locality':
-                          $cityField.val(component.long_name);
-                          break;
-                      case 'administrative_area_level_2':
-                          $countyField.val(component.long_name);
-                          break;
-                      case 'postal_code':
-                          $postcodeField.val(component.long_name);
-                          break;
-                      case 'country':
-                          $countryField.val(component.long_name);
-                          break;
-                  }
-              });
-
-              // Set latitude and longitude
-              if ($latField.length && $lngField.length) {
-                  $latField.val(place.geometry.location.lat());
-                  $lngField.val(place.geometry.location.lng());
-                  console.log(`Latitude set to: ${$latField.val()}, Longitude set to: ${$lngField.val()}`);
-              } else {
-                  console.error(`Latitude or Longitude field not found for base ID: "${baseId}". Expected IDs: "${latFieldId}" and "${lngFieldId}".`);
-              }
-          } else {
-              console.log(`No geometry available for input: '${place.name}'`);
-          }
-      });
-  });
+    ensureDependenciesAndInitialize();
 });
